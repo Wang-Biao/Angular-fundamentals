3b873f5b65c4fde6e883c1cfdff1bec0320d23e0
Biao Wang <biao@oculys.com>
2019-07-23 21:48:30 -0400

    Angular-fundamentals functioning example from Edx courses

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { GitSearchComponent } from './git-search/git-search.component';
 
 
-const routes: Routes = [];
+
+const appRoutes: Routes = [
+
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  // {
+  //   path: 'search',
+  //   component: GitSearchComponent,
+  //   data: {
+  //     title: 'Git Search'
+  //   }
+  // },
+
+  {
+    path: 'search',
+    redirectTo: '/search/angular',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'search/:query',
+    component: GitSearchComponent,
+    data: { title: 'Git Search' }
+  },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,48 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {HttpClientModule} from '@angular/common/http';
+
+import {GitSearchService} from './git-search.service';
+import { GitSearchComponent } from './git-search/git-search.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+import {RouterModule, Routes} from '@angular/router'; // routing
+// we can move the routes part to app-routing.module.ts
+
+// const appRoutes: Routes = [
+//   { path: '', 
+//     component: HomePageComponent 
+//   },
+//   { path: 'search',      
+//     component: GitSearchComponent,
+//     data: { 
+//       title: 'Git Search' 
+//     } 
+//   },
+//   { path: '**', component: NotFoundComponent }
+// ];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitSearchComponent,
+    HomePageComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+    // RouterModule.forRoot(
+    //   appRoutes
+    // )
   ],
-  providers: [],
+  providers: [GitSearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
